2bee267f116edca9a6c36800ead92b654bcdaa49
Peluc0n <crespodavidceballos@gmail.com>
2025-03-11 17:44:25 +0100

    Add files via upload

---
+++ BACK-END/Login.php
@@ -0,0 +1,16 @@
+<?php // login.php
+$host = 'localhost'; // Change as necessary
+$data = 'concesionario'; // Change as necessary
+$user = 'root'; // Change as necessary
+$pass = ''; // Change as necessary
+$chrs = 'utf8mb4';
+$attr = "mysql:host=$host;dbname=$data;charset=$chrs";
+$opts =
+[
+PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+PDO::ATTR_EMULATE_PREPARES => false,
+];
+?>
+
+
